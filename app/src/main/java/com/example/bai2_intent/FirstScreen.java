c7694f230c30ce490208ecc341002dd34edb6348
TranNhatQuang2405 <trannhatquang2405@gmail.com>
2022-01-13 20:20:10 +0700

    Them chuc nang gui thong tin giua cac activity

--- app/src/main/java/com/example/bai2_intent/FirstScreen.java
+++ app/src/main/java/com/example/bai2_intent/FirstScreen.java
@@ -3,9 +3,11 @@ package com.example.bai2_intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstScreen extends AppCompatActivity {
 
@@ -14,7 +16,30 @@ public class FirstScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
 
-        Intent serviceIntent = new Intent(this, ServiceEx.class);
-        startService(serviceIntent);
+        //Truyền dữ liệu
+
+
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setText("Vào GOOGLE");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstScreen.this, SecondScreen.class);
+                intent.putExtra("Key", "This is value of key one");
+                intent.putExtra("Key2", "This is value of key two");
+                int REQUEST_CODE = 9;
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 9) {
+            if (data.hasExtra("returnKey1")) {
+                Toast.makeText(this, data.getExtras().getString("returnKey1"), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/bai2_intent/SecondScreen.java
+++ app/src/main/java/com/example/bai2_intent/SecondScreen.java
@@ -2,7 +2,11 @@ package com.example.bai2_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondScreen extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class SecondScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+
+        Bundle extras = getIntent().getExtras();
+        String data = extras.getString("Key");
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+    @Override
+    public void finish(){
+        Intent data =  new Intent();
+        data.putExtra("returnKey1","Trả về giá trị 1");
+        data.putExtra("returnKey2","Trả về giá trị 2");
+        setResult(RESULT_OK, data);
+        super.finish();
+    }
+
 }
